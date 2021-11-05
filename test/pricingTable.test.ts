fc973c3f0194e45d747bc58bf5b55c858d29140c
Aboudjem <boudjemaa.adam@gmail.com>
2021-11-05 16:50:17 +0400

    :white_check_mark: update tests to fit new requirements

--- test/pricingTable.test.ts
+++ test/pricingTable.test.ts
@@ -11,42 +11,38 @@ describe("PricingTable", function () {
   });
 
   it("Should add a pricing Item", async () => {
-    const tx = await pricingTable
-      .addPricingItem(1, 1, 1, 1, 1, 1)
+    await pricingTable
+      .addPricingItem("0x606A", 20, 60, 90, 65, 12, 5000, 10000)
       .then((tx) => tx.wait());
-    const pricingId = parseInt(tx.logs[0].data);
-    expect(pricingId).to.equal(1);
   });
 
-  it("Should add second pricing Item", async () => {
-    const tx = await pricingTable
-      .addPricingItem(1, 1, 1, 1, 1, 1)
+  it("Should add a pricing Item", async () => {
+    await pricingTable
+      .addPricingItem("0x01A2", 1, 1, 1, 1, 1, 1, 1)
       .then((tx) => tx.wait());
-    const pricingId = parseInt(tx.logs[0].data);
-    expect(pricingId).to.equal(2);
   });
 
   it("Should return a pricingItem", async () => {
-    const pricingItem = await pricingTable.getPricingItem(1);
+    const pricingItem = await pricingTable.getPricingItem("0x01a2");
 
     expect(pricingItem.minTenure).to.equal(1);
     expect(pricingItem.maxTenure).to.equal(1);
     expect(pricingItem.maxAdvancedRatio).to.equal(1);
-    expect(pricingItem.minDiscountRange).to.equal(1);
+    expect(pricingItem.minDiscountFee).to.equal(1);
     expect(pricingItem.minFactoringFee).to.equal(1);
     expect(pricingItem.minAmount).to.equal(1);
   });
 
   it("Should remove a pricingItem", async () => {
-    await pricingTable.removePricingItem(1);
+    await pricingTable.removePricingItem("0x01a2");
   });
 
   it("Should remove 0 if pricingItem has been deleted", async () => {
-    const pricingItem = await pricingTable.getPricingItem(1);
+    const pricingItem = await pricingTable.getPricingItem("0x01a2");
     expect(pricingItem.minTenure).to.equal(0);
     expect(pricingItem.maxTenure).to.equal(0);
     expect(pricingItem.maxAdvancedRatio).to.equal(0);
-    expect(pricingItem.minDiscountRange).to.equal(0);
+    expect(pricingItem.minDiscountFee).to.equal(0);
     expect(pricingItem.minFactoringFee).to.equal(0);
     expect(pricingItem.minAmount).to.equal(0);
   });
