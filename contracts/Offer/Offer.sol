330a076a90ed73d9869532adcbfc450a6e9f08b5
Aboudjem <boudjemaa.adam@gmail.com>
2021-12-01 17:40:02 +0400

    :zap: add IERC20 + SafeERC20

--- contracts/Offer/Offer.sol
+++ contracts/Offer/Offer.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.10;
 import "./IOffer.sol";
 import "../PricingTable/IPricingTable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 /// @title Offer
 /// @author Polytrade
