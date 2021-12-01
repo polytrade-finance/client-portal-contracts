c331b64aa305a4226df4c99463aa318810763447
Aboudjem <boudjemaa.adam@gmail.com>
2021-12-01 15:11:28 +0400

    :art: add natspec for _checkParams

--- contracts/Offer/Offer.sol
+++ contracts/Offer/Offer.sol
@@ -131,6 +131,17 @@ contract Offers is IOffer, Ownable {
         emit ReserveRefunded(offerId, refunded.netAmount);
     }
 
+    /**
+     * @notice check if params fit with the pricingItem
+     * @dev checks every params and returns a custom Error
+     * @param pricingId, Id of the pricing Item
+     * @param tenure, tenure expressed in number of days
+     * @param advanceFee, ratio for the advance Fee
+     * @param discountFee, ratio for the discount Fee
+     * @param factoringFee, ratio for the factoring Fee
+     * @param invoiceAmount, amount for the invoice
+     * @param availableAmount, amount for the available amount
+     */
     function _checkParams(
         bytes2 pricingId,
         uint8 tenure,
