f0f2e53aba5b76ef9abd7d7583b5d1ecfe331700
Aboudjem <boudjemaa.adam@gmail.com>
2021-12-15 00:26:31 +0400

    :truck: rename IPriceFeedUSDC to IPriceFeeds

--- contracts/Chainlink/IPriceFeedUSDC.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
-
-interface IPriceFeedUSDC {
-    function getPrice() external view returns (uint);
-
-    function getDecimals() external view returns (uint);
-}
